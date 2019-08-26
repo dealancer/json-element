c9969c7042a2f3b528afda49edba66bd587c1dcd
Vadym Myrgorod <dealancer@gmail.com>
2019-08-26 12:53:53 -0400

    Add Demo webpack config

---
+++ demo.webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './demo/src/js/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+          publicPath: 'dist',
+        }
+      },
+      {
+        test: /.\/src\/css\/style.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: [/brutusin-json-forms\/dist\/css\/brutusin-json-forms.min.css$/i, /jsoneditor\/dist\/jsoneditor.min.css$/i, /.\/src\/css\/component.css$/i],
+        use: ['to-string-loader', 'css-loader'],
+      },
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'demo/dist'),
+    filename: 'bundle.js'
+  }
+};
